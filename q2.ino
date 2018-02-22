2eef84a5632279af247dbda6ea56fac8c18b54e4
nareman tarek <36613702+naremanatrek@users.noreply.github.com>
2018-02-22 12:36:32 +0200

    Add files via upload

---
+++ q2.ino
@@ -0,0 +1,32 @@
+const int led1 = 4;
+const int led2 = 5;
+const int led3 = 6;
+const int led4 = 7;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led1 , OUTPUT);
+pinMode(led2 , OUTPUT);
+pinMode(led3 , OUTPUT);
+pinMode(led4 , OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(led1 , HIGH);
+delay(1000);
+digitalWrite(led1 , LOW);
+delay(3000);
+digitalWrite(led2 , HIGH);
+delay(1000);
+digitalWrite(led2 , LOW);
+delay(3000);
+digitalWrite(led3 , HIGH);
+delay(1000);
+digitalWrite(led3 , LOW);
+delay(3000);
+digitalWrite(led4 , HIGH);
+delay(1000);
+digitalWrite(led4 , LOW);
+delay(3000);
+
+}
