11eca1083b1af1903d62dcb57f0b7211130e5fa6
Yara Fekry <36644314+YaraFekri@users.noreply.github.com>
2018-03-13 14:27:14 +0200

    Add files via upload

---
+++ ultrasonic_w_motor.ino
@@ -0,0 +1,47 @@
+const int trigPin = 9;
+const int echoPin = 10;
+const int pwm = 2 ;  //initializing pin 2 as pwm
+const int in_1 = 7 ;
+const int in_2 = 8 ;
+//For providing logic to L298 IC to choose the direction of the DC motor 
+// defines variables
+long duration;
+int distance;
+void setup() {
+pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+Serial.begin(9600); // Starts the serial communication
+pinMode(pwm,OUTPUT) ;    //we have to set PWM pin as output
+pinMode(in_1,OUTPUT) ;  //Logic pins are also set as output
+pinMode(in_2,OUTPUT) ;
+}
+void loop() {
+digitalWrite(in_1,HIGH) ;
+digitalWrite(in_2,LOW) ;
+analogWrite(pwm,255) ;
+// Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+distance= duration*0.034/2; // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the object we
+  // take half of the distance travelled.
+Serial.print(distance);
+if  (distance < 10)
+{ 
+//For brake
+digitalWrite(in_1,HIGH) ;
+digitalWrite(in_2,HIGH) ;
+delay(2000) ;
+//For Anti Clock-wise motion - IN_1 = LOW , IN_2 = HIGH
+digitalWrite(in_1,LOW) ;
+digitalWrite(in_2,HIGH) ;
+delay(2000) ;
+}
+ }
