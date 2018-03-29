a7d18bb3001c78efc2b5b07178952d96d0dee0e7
Yara Fekry <36644314+YaraFekri@users.noreply.github.com>
2018-03-29 09:39:29 +0200

    Add files via upload

---
+++ Arduino Cooling Pad/coolestcoolerforcooling.ino
@@ -0,0 +1,29 @@
+void setup() {
+  pinMode(7, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+}
+
+void loop() {
+  int val = digitalRead(7);
+  int val2 = digitalRead(6);
+  int val3 = digitalRead(5);
+  int val4 = digitalRead(4);
+  if (val == LOW) {
+      analogWrite(3, 255);
+  delay(1000);
+  }
+  if (val2 == LOW) {
+     analogWrite(3, 191);
+  delay(1000);
+  }
+  if (val3 == LOW) {
+    analogWrite(3, 127);
+  delay(1000);
+  }
+  if (val4 == LOW) {
+    analogWrite(3, 64);
+  delay(1000);
+  }
+}
